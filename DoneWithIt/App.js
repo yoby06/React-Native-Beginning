a01a9d84eff26ec3e6e08b00b1436507f78fab74
yoby06 <100288804+yoby06@users.noreply.github.com>
2023-01-26 11:31:57 +0300

    React-Native-Last-Updated
    By programming with mosh

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,32 +1,13 @@
 import {View} from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 
 export default function App() {  
   return (  
     <>
-
-    <View style={{
-      backgroundColor:"#fff",
-      flex:1,
-      flexDirection:"column",
-      justifyContent:"center"}}>
-    <View style={{
-      backgroundColor:"dodgerblue",
-      width:100,
-      height:100,
-    }}/>
-
-    <View style={{
-      backgroundColor:"gold",
-      width:100,
-      height:100,    }}/>
+        <ViewImageScreen/>
     
-
-    <View style={{
-      backgroundColor:"tomato",
-      width:100,
-      height:100,    }}/>
-    </View>
     </>
   );
 }
